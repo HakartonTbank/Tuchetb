a66b98b8ae72e98a3214f2194dead970b7816ae3
Vladik <vlad.bondarev.ru@gmail.com>
2024-11-23 00:56:41 +0300

    подключил аутентификацию + real time bd для хранения пользователей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth:23.1.0")
+    implementation ("com.google.firebase:firebase-database:21.0.0")
 }
