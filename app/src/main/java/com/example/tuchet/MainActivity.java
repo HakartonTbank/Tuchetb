99dce68cd639c90518f276810ada9f9bd2a91635
cvcvcvtx <vitalik090806@gmail.com>
2024-11-22 22:37:53 +0300

    джава классы для фрагментов

--- app/src/main/java/com/example/tuchet/MainActivity.java
+++ app/src/main/java/com/example/tuchet/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.tuchet;
 
+import static android.app.PendingIntent.getActivity;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
     //йоу
 
+    ImageView homeIV;
+    ImageView profileIV;
+    ImageView assistentIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +27,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        replaceFragment(new FragmentHome());
+        homeIV = findViewById(R.id.home_button);
+        profileIV = findViewById(R.id.profile_button);
+        assistentIV = findViewById(R.id.assistent_button);
+
+
+
 
     }
 
-    /*
+
     private void replaceFragment(Fragment fragment){
-        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentManager fragmentManager = this.getSupportFragmentManager(); //getActivity()
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.place_holder, fragment);
         fragmentTransaction.commit();
+    }
 
-     */
+
 }
 
 
