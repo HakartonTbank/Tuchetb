f3f8dfc8456d41650aed96ddac50222114cd37cd
cvcvcvtx <vitalik090806@gmail.com>
2024-11-23 02:08:45 +0300

    заставляем пользователя заполнять поля регистрации (

--- app/src/main/java/com/example/tuchet/LoginActivity.java
+++ app/src/main/java/com/example/tuchet/LoginActivity.java
@@ -3,25 +3,42 @@ package com.example.tuchet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText login;
+    EditText pass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_activity);
+        login = findViewById(R.id.plain_text_login);
+        pass = findViewById(R.id.plain_text_pass);
     }
 
 
 
     public void toMain(View view) {
 
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
+        if (login.getText().toString().isEmpty() || pass.getText().toString().isEmpty()){
+            Toast.makeText(this, "Заполните все поля.",Toast.LENGTH_LONG).show();
+        }else {
+            //тут всякая дичь с бд
+
+
+
+
+            //это переход это не трогай
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
 }
--- app/src/main/java/com/example/tuchet/RegisterActivity.java
+++ app/src/main/java/com/example/tuchet/RegisterActivity.java
@@ -3,23 +3,47 @@ package com.example.tuchet;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    EditText login;
+    EditText pass;
+    EditText name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.register_activity);
+
+        login = findViewById(R.id.plain_text_login);
+        pass = findViewById(R.id.plain_text_pass);
+        name = findViewById(R.id.plain_text_name);
+
+
     }
 
     public void toMain(View view) {
 
-        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-        startActivity(intent);
+        if (login.getText().toString().isEmpty() || pass.getText().toString().isEmpty() || name.getText().toString().isEmpty()){
+            Toast.makeText(this, "Заполните все поля.",Toast.LENGTH_LONG).show();
+        }else {
+            //тут всякая дичь с бд
+
+
+
+
+            //это переход это не трогай
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 }
