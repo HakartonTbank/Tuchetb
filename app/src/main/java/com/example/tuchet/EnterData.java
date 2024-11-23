0b5342f992ee6899cf70acf678cc3c0ab0778809
Your Name <jarmalovich@gmail.com>
2024-11-23 13:55:30 +0300

    добавлено окно ввода

--- app/src/main/java/com/example/tuchet/EnterData.java
+++ app/src/main/java/com/example/tuchet/EnterData.java
@@ -1,18 +1,58 @@
 package com.example.tuchet;
 
+import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class EnterData extends AppCompatActivity {
+
+    EditText Money;
+
+    String[] types = {"Food", "HouseAndCS", "Medicine", "OptionalExpenses", "Transport"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.enter_data_activity);
+        Money = findViewById(R.id.enterMoney);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, types);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+        spinner.setSelection(2);
+
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Toast.makeText(getBaseContext(), "Position =" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0){
+
+            }
+        });
 
     }
 
