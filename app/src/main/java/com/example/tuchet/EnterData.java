727116a4021a870d8a4de63601931685713e6bef
Your Name <jarmalovich@gmail.com>
2024-11-23 15:10:01 +0300

    дата есть, но формат неправильный(

--- app/src/main/java/com/example/tuchet/EnterData.java
+++ app/src/main/java/com/example/tuchet/EnterData.java
@@ -11,10 +11,20 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,12 +35,17 @@ public class EnterData extends AppCompatActivity {
 
     String[] types = {"Food", "HouseAndCS", "Medicine", "OptionalExpenses", "Transport"};
 
+    TextView currentDateTime;
+    Calendar dateAndTime=Calendar.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.enter_data_activity);
         Money = findViewById(R.id.enterMoney);
+        currentDateTime = findViewById(R.id.currentDateTime);
+        setInitialDateTime();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, types);
@@ -56,4 +71,32 @@ public class EnterData extends AppCompatActivity {
 
     }
 
+    public void setDate(View v) {
+        new DatePickerDialog(EnterData.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
+
+        currentDateTime.setText(DateUtils.formatDateTime(this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
+    }
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
 }
+
+
