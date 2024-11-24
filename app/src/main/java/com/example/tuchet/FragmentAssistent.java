e488301c531bc66a3354ec226c172f13562acfff
cvcvcvtx <vitalik090806@gmail.com>
2024-11-24 09:35:22 +0300

    fragment assistent + нужен  дебаг

--- app/src/main/java/com/example/tuchet/FragmentAssistent.java
+++ app/src/main/java/com/example/tuchet/FragmentAssistent.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
@@ -26,6 +27,7 @@ public class FragmentAssistent extends Fragment {
     Calendar dateAndTime1 = Calendar.getInstance();
     Calendar dateAndTime2 = Calendar.getInstance();
     TextView textDates, textSpend, textIncome;
+    Button datePicker1, datePicker2, enterDates;
 
     @SuppressLint("MissingInflatedId")
     @Nullable
@@ -37,8 +39,88 @@ public class FragmentAssistent extends Fragment {
         textDates = v.findViewById(R.id.text_dates);
         textSpend = v.findViewById(R.id.textSpend);
         textIncome = v.findViewById(R.id.textIncome);
+        datePicker1 = v.findViewById(R.id.first_date_picker);
+        datePicker2 = v.findViewById(R.id.second_date_picker);
+        enterDates = v.findViewById(R.id.enter_date_button);
         textSpend.setVisibility(View.INVISIBLE);
         textIncome.setVisibility(View.INVISIBLE);
+
+
+        datePicker1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("my_logs", "setDate");
+                new DatePickerDialog(requireContext(), d1,
+                        dateAndTime1.get(Calendar.YEAR),
+                        dateAndTime1.get(Calendar.MONTH),
+                        dateAndTime1.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+
+        datePicker2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(requireContext(), d2,
+                        dateAndTime2.get(Calendar.YEAR),
+                        dateAndTime2.get(Calendar.MONTH),
+                        dateAndTime2.get(Calendar.DAY_OF_MONTH))
+                        .show();
+            }
+        });
+
+        enterDates.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint({"SetTextI18n", "DefaultLocale"})
+            @Override
+            public void onClick(View v) {
+                if (flag1 && flag2) {
+                    textDates.setText("Расходы c " + DateUtils.formatDateTime(requireActivity(),
+                            dateAndTime1.getTimeInMillis(),
+                            DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR) + "\nпо " + DateUtils.formatDateTime(requireActivity(),
+                            dateAndTime2.getTimeInMillis(),
+                            DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR) + ".");
+                    long unix1 = (date1.getTime() / 86400000);
+                    long unix2 = (date2.getTime() / 86400000);
+
+                    DataOperation sum = new DataOperation();
+
+                    for (int i = 0; i < UserData.dataList.size(); i++) {
+                        DataOperation d = UserData.dataList.get(i);
+                        if ((d.unixDate / 86400000 >= unix1) && (d.unixDate / 86400000 <= unix2)) {
+                            sum.food += d.food;
+                            sum.cash += d.cash;
+                            sum.nonCash += d.nonCash;
+                            sum.wage += d.wage;
+                            sum.other += d.other;
+                            sum.scholarship += d.scholarship;
+                            sum.houseAndCS += d.houseAndCS;
+                            sum.medicine += d.medicine;
+                            sum.optionalExpenses += d.optionalExpenses;
+                            sum.transport += d.transport;
+                        }
+                    }
+
+                    Double q = (double) sum.cash;
+                    long totalSpend = sum.food + sum.houseAndCS + sum.medicine + sum.optionalExpenses + sum.transport;
+                    long totalIncome = sum.cash + sum.nonCash + sum.wage + sum.other + sum.scholarship;
+
+
+
+                    textSpend.setText("Расходы:\nПитание - " + String.format("%.3f",((double) sum.food / totalSpend) * 100) + "%\nТранспорт - " + String.format("%.3f",
+                            ((double) sum.transport / totalSpend) * 100) + "%\nЗдоровье - " + String.format("%.3f",((double) sum.medicine / totalSpend) * 100) + "%\nОплата налогов и счетов - " + String.format("%.3f",
+                            ((double) sum.houseAndCS / totalSpend) * 100) + "%\nОпциональные траты - " + String.format("%.3f",((double) sum.optionalExpenses / totalSpend) * 100) +"%");
+
+                    textIncome.setText("Доходы:\nНаличные - " + String.format("%.3f",((double) sum.cash / totalSpend) * 100) + "%\nБезналичные - " + String.format("%.3f",
+                            ((double) sum.nonCash / totalSpend) * 100) + "%\nСтипендия - " + String.format("%.3f",((double) sum.scholarship / totalSpend) * 100) + "%\nЗароботная плата - " + String.format("%.3f",
+                            ((double) sum.wage / totalSpend) * 100) + "%\nДругое - " + String.format("%.3f",((double) sum.other / totalSpend) * 100) + "%");
+
+                    textSpend.setVisibility(View.VISIBLE);
+                    textIncome.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
+
 
         return v;
     }
@@ -49,77 +131,6 @@ public class FragmentAssistent extends Fragment {
         date2 = dateAndTime2.getTime();
     }
 
-    @SuppressLint("SetTextI18n")
-    public void onEnterDatesClick(View v){
-        /*
-        if (flag1 && flag2) {
-            textDates.setText("Расходы c " + DateUtils.formatDateTime(requireActivity(),
-                    dateAndTime1.getTimeInMillis(),
-                    DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR) + "\nпо " + DateUtils.formatDateTime(requireActivity(),
-                    dateAndTime2.getTimeInMillis(),
-                    DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR) + ".");
-            long unix1 = (date1.getTime() / 86400000);
-            long unix2 = (date2.getTime() / 86400000);
-
-            DataOperation sum = new DataOperation();
-
-            for (int i = 0; i < UserData.dataList.size(); i++) {
-                DataOperation d = UserData.dataList.get(i);
-                if ((d.unixDate / 86400000 >= unix1) && (d.unixDate / 86400000 <= unix2)) {
-                    sum.food += d.food;
-                    sum.cash += d.cash;
-                    sum.nonCash += d.nonCash;
-                    sum.wage += d.wage;
-                    sum.other += d.other;
-                    sum.scholarship += d.scholarship;
-                    sum.houseAndCS += d.houseAndCS;
-                    sum.medicine += d.medicine;
-                    sum.optionalExpenses += d.optionalExpenses;
-                    sum.transport += d.transport;
-                }
-            }
-
-            long totalSpend = sum.food + sum.houseAndCS + sum.medicine + sum.optionalExpenses + sum.transport;
-            long totalIncome = sum.cash + sum.nonCash + sum.wage + sum.other + sum.scholarship;
-
-            textSpend.setText("Расходы:\nПитание - " + Long.toString((sum.food / totalSpend) * 100) + "\nТранспорт - " + Long.toString(
-                    (sum.transport / totalSpend) * 100) + "\nЗдоровье - " + Long.toString((sum.medicine / totalSpend) * 100) + "\nОплата налогов и счетов - " + Long.toString(
-                    (sum.houseAndCS / totalSpend) * 100) + "\nОпциональные траты - " + Long.toString((sum.optionalExpenses / totalSpend) * 100));
-
-            textIncome.setText("Доходы:\nНаличные - " + Long.toString((sum.cash / totalIncome) * 100) + "\nБезналичные - " + Long.toString(
-                    (sum.nonCash / totalIncome) * 100) + "\nСтипендия - " + Long.toString((sum.scholarship / totalIncome) * 100) + "\nЗароботная плата - " + Long.toString(
-                    (sum.wage / totalIncome) * 100) + "\nДругое - " + Long.toString((sum.other / totalIncome) * 100));
-
-            textSpend.setVisibility(View.VISIBLE);
-            textIncome.setVisibility(View.VISIBLE);
-
-        }
-
-         */
-    }
-
-
-    public void setDate1(View v) {
-        /*
-        new DatePickerDialog(getContext(), d1,
-                dateAndTime1.get(Calendar.YEAR),
-                dateAndTime1.get(Calendar.MONTH),
-                dateAndTime1.get(Calendar.DAY_OF_MONTH))
-                .show();
-
-         */
-    }
-
-    public void setDate2(View v) {
-        /*
-        new DatePickerDialog(getContext(), d2,
-                dateAndTime2.get(Calendar.YEAR),
-                dateAndTime2.get(Calendar.MONTH),
-                dateAndTime2.get(Calendar.DAY_OF_MONTH))
-                .show();
-
-         */
-    }
 
 
     DatePickerDialog.OnDateSetListener d1=new DatePickerDialog.OnDateSetListener() {
